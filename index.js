37a36c48574b8490fd90843eb0cde564f092bc78
Kamil <kamilrogozinski29@gmail.com>
2023-01-05 13:22:10 +0000

    Added a grid system and user able to pick what size the gird will be

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const main = document.getElementById('container');
-// let divNumber = prompt('how many divs')
-function addDives() {
-    for(let i=0;i<=50;i++) {
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('divs');
-        // newDiv.textContent=('Hello there')
-        main.append(newDiv);
+let divNumber = prompt('how many divs')
+function addDives(divNumber) {
+    for(let i=0;i<divNumber;i++) {
+        for(let x=divNumber;x>0;x--){
+            let col = document.createElement('div');
+            col.classList.add('row');
+            main.style.gridTemplateColumns = `repeat(${divNumber},1fr)`;
+            main.style.gridTemplateRows= `repeat(${divNumber},1fr)`;
+            main.append(col);
+            col.addEventListener('mouseover',() => {
+                col.classList.toggle('colour')
+            })
+        }
     }
 }
 
-addDives();
+
+
+addDives(divNumber);
+
