1876cdbc8a7b19da72cffe76a6dfff7ade16ceb9
Kamil <kamilrogozinski29@gmail.com>
2023-01-06 08:54:14 +0000

    Added responsive grid layout and random generated colours

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const main = document.getElementById('container');
-let divNumber = prompt('how many divs')
+let divNumber = 10;
 function addDives(divNumber) {
     for(let i=0;i<divNumber;i++) {
         for(let x=divNumber;x>0;x--){
             let col = document.createElement('div');
             col.classList.add('row');
-            main.style.gridTemplateColumns = `repeat(${divNumber},1fr)`;
-            main.style.gridTemplateRows= `repeat(${divNumber},1fr)`;
+            col.style.width = `500/${divNumber}px`
+            col.style.height = `500/${divNumber}px`
+            main.style.gridTemplateColumns = `repeat(${divNumber},1fr) `;
+            main.style.gridTemplateRows= `repeat(${divNumber},1fr) `;
             main.append(col);
             col.addEventListener('mouseover',() => {
-                col.classList.toggle('colour')
+                col.style.backgroundColor = `${randColor()}`;
             })
         }
     }
 }
 
-
+// creates a random color each time 
+const randColor = () =>  {
+    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
 
 addDives(divNumber);
 
